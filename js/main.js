329258c2d4842dc6e0900b9c4013298f0c4ee463
sakshi jain <sjain302086@gmail.com>
2022-03-04 16:20:27 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+<script src="bootstrap.bundle.min.js"></script>
+var nav = document.querySelector('nav');
+  
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 100) {
+    nav.classList.add('bg-dark', 'shadow');
+  } else {
+    nav.classList.remove('bg-dark', 'shadow');
+  }
+});
